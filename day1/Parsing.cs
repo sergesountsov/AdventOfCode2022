c8d999a5ba154b2ff73c2ee5e4163f408e1ec787
Serge Sountsov <serge.sountsov@gmail.com>
2022-12-01 14:15:31 -0800

    Day1 part1

---
+++ day1/Parsing.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common;
+
+public static class Parsing
+{
+    public static IEnumerable<string?> ParseSingleLine(string fileName)
+    {
+        using var stream = File.OpenText(fileName);
+        while (!stream.EndOfStream)
+        {
+            yield return stream.ReadLine();
+        }
+    }
+
+    public static IEnumerable<IEnumerable<string>> ChunkBy(this IEnumerable<string?> seq, Func<string?, bool> endChunk)
+    {
+        List<string> result = new();
+        foreach (var str in seq)
+        {
+            if (endChunk(str))
+            {
+                yield return result;
+                result = new();
+            }
+            else
+            {
+                result.Add(str);
+            }
+        }
+        if (result.Count > 0) yield return result;
+    }
+}
---
+++ day1/Program.cs
@@ -0,0 +1,6 @@
+﻿using Common;
+
+var input = Parsing.ParseSingleLine("input.txt");
+var part1 = input.ChunkBy(String.IsNullOrEmpty).Max(chunk => chunk.Aggregate(0, (total, next) => total + int.Parse(next)));
+Console.WriteLine(part1!);
+
