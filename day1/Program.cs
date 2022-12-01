5069a062acc6eea914faff6d5a59bf015307e51a
Serge Sountsov <serge.sountsov@gmail.com>
2022-12-01 14:23:53 -0800

    Part 2

--- day1/Program.cs
+++ day1/Program.cs
@@ -3,5 +3,7 @@
 var input = Parsing.ParseSingleLine("input.txt");
 var totalPerElf = input.ChunkBy(String.IsNullOrEmpty).Select(chunk => chunk.Aggregate(0, (total, next) => total + int.Parse(next!)));
 var part1 = totalPerElf.Max();
-Console.WriteLine(part1!);
+Console.WriteLine($"Part 1 result = {part1!}");
+var part2 = totalPerElf.Order().TakeLast(3).Aggregate(0, (total, next) => total + next);
+Console.WriteLine($"Part 2 result = {part2!}");
 
