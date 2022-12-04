046ca71ad035d37d711312542cd27f7f56649e74
sergesountsov <serge.sountsov@gmail.com>
2022-12-04 19:01:04 +0000

    Part 1 done

--- day4/Program.cs
+++ day4/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var input = File.ReadAllLines("input.txt");
+var ranges = input.Select(s => 
+{
+    var parts = s.Split(",");
+    return (new Range(parts[0]), new Range(parts[1]));
+});
+
+var part1 = ranges.Aggregate(0, (total, next) => 
+    total + ((next.Item1.Contains(next.Item2) || next.Item2.Contains(next.Item1)) ? 1 : 0)
+);
+
+Console.WriteLine("Part 1 result: {0}", part1);
+
+record Range(int Start, int End) 
+{
+    public Range(string rangeStr) : this(0,0)
+    {
+        string[] parts = rangeStr.Split("-");
+        Start = int.Parse(parts[0]);
+        End = int.Parse(parts[1]);
+    }
+
+    public bool Contains(Range other) =>    
+        other.Start >= this.Start && other.End <= this.End;
+}
