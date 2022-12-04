49f6521e62e261346e7e139533594d27a750da9f
sergesountsov <serge.sountsov@gmail.com>
2022-12-04 19:56:24 +0000

    Day 4 part 2

--- day4/Program.cs
+++ day4/Program.cs
@@ -11,6 +11,11 @@ var part1 = ranges.Aggregate(0, (total, next) =>
 
 Console.WriteLine("Part 1 result: {0}", part1);
 
+var part2 = ranges.Aggregate(0, (total, next) => 
+    total + (next.Item1.Overlaps(next.Item2) ? 1 : 0)
+);
+
+Console.WriteLine("Part 2 result: {0}", part2);
 record Range(int Start, int End) 
 {
     public Range(string rangeStr) : this(0,0)
@@ -22,4 +27,9 @@ record Range(int Start, int End)
 
     public bool Contains(Range other) =>    
         other.Start >= this.Start && other.End <= this.End;
+
+    public bool Includes(int i) => i >= Start && i <= End;
+        
+    public bool Overlaps(Range other) =>
+       other.Includes(this.Start) || other.Includes(this.End) || this.Includes(other.Start) || this.Includes(other.End);
 }
